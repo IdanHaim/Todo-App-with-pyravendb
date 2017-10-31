867178d5569fdc5c2e06b00ceefb00cb9e860e71
Idan Haim Shalom <haimdude@gmail.com>
2017-10-31 17:04:44 +0200

    Add completed page, pagination and split methods

--- app.py
+++ app.py
@@ -1,37 +1,58 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_paginate import Pagination, get_page_parameter
 from database import Database
-from datetime import datetime
 from models import Todo
+from datetime import datetime
 import re
 
 app = Flask(__name__)
 db = Database()
 
+POSTS_PER_PAGE = 10
+
+
+@app.template_filter()
+def format_id(text):
+    return re.match(pattern=r"todos\/(\d+)-", string=text, flags=re.IGNORECASE).group(1)
+
 
 @app.route('/')
 def index():
     with db.document_store.open_session() as session:
-        todos = list(session.query(object_type=Todo, nested_object_types={"pub_date": datetime},
-                                   wait_for_non_stale_results=True).order_by_descending("pub_date"))
+        page = request.args.get(get_page_parameter(), type=int, default=1)
 
-        incomplete = [todo for todo in todos if not todo.done]
-        complete = [todo for todo in todos if todo.done]
+        incomplete_items, stats = list(session.query(with_statistics=True).order_by_descending(
+            "pub_date").where(done=False).take(POSTS_PER_PAGE).skip((page - 1) * POSTS_PER_PAGE))
+        incomplete_total_result = stats['TotalResults']
 
-        for todo in todos:
-            todo.temp_id = re.match(pattern=r"todos\/(\d+)-", string=todo.Id, flags=re.IGNORECASE).group(1)
-    return render_template('index.html', todos=todos, incomplete = incomplete, complete=complete)
+        pagination = Pagination(page=page, total=incomplete_total_result, record_name='incomplete todos')
+    return render_template('index.html', incomplete_items=incomplete_items, pagination=pagination)
 
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/completed')
+def completed_items():
+    with db.document_store.open_session() as session:
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        completed_items, stats = list(session.query(with_statistics=True).order_by_descending(
+            "complete_date").where(done=True).take(POSTS_PER_PAGE).skip((page - 1) * POSTS_PER_PAGE))
+        complete_total_result = stats['TotalResults']
+
+        pagination = Pagination(page=page, total=complete_total_result, record_name='completed todos')
+    return render_template('completed.html', completed_items=completed_items, pagination=pagination)
+
+
+@app.route('/new', methods=['POST'])
+def create():
+    with db.document_store.open_session() as session:
+        title = request.form['title']
+        text = request.form['text']
+        session.store(Todo(title=title, text=text))
+        session.save_changes()
+    return redirect(url_for('index'))
+
+
+@app.route('/new', methods=['GET'])
 def new():
-    if request.method == 'POST':
-        with db.document_store as store:
-            with store.open_session() as session:
-                title = request.form['title']
-                text = request.form['text']
-                session.store(Todo(title=title, text=text))
-                session.save_changes()
-        return redirect(url_for('index'))
     return render_template('new.html')
 
 
@@ -39,23 +60,41 @@ def new():
 def complete():
     id = request.args.get('id')
     with db.document_store.open_session() as session:
-        todo = session.load(key_or_keys=id, nested_object_types={"pub_date": datetime})
+        todo = session.load(id)
         todo.done = True
+        todo.complete_date = datetime.utcnow()
         session.save_changes()
     return redirect(url_for('index'))
 
 
-@app.route('/todos/', methods=['GET', 'POST'])
+@app.route('/incomplete')
+def incomplete():
+    id = request.args.get('id')
+    with db.document_store.open_session() as session:
+        todo = session.load(id)
+        todo.done = False
+        todo.complete_date = None
+        session.save_changes()
+    return redirect(url_for('index'))
+
+
+@app.route('/todos', methods=['POST'])
 def update():
     id = request.args.get('id')
     with db.document_store.open_session() as session:
         todo = session.load(id)
-        if request.method == 'GET':
-            return render_template('view.html', todo=todo)
         todo.title = request.form['title']
         todo.text = request.form['text']
         session.save_changes()
         return redirect(url_for('index'))
+
+
+@app.route('/todos', methods=['GET'])
+def view():
+    id = request.args.get('id')
+    with db.document_store.open_session() as session:
+        todo = session.load(id)
+        return render_template('view.html', todo=todo)
 
 
 @app.route('/delete')
--- models.py
+++ models.py
@@ -1,10 +1,18 @@
 from datetime import datetime
+from pyravendb.tools.utils import Utils
 
 
 class Todo:
-    def __init__(self, title=None, text=None):
+    def __init__(self, title=None, text=None, pub_date=None, complete_date=None):
         self.Id = None
         self.title = title
         self.text = text
         self.done = False
-        self.pub_date = datetime.utcnow()
+        if not pub_date:
+            self.pub_date = datetime.utcnow()
+        else:
+            self.pub_date = Utils.string_to_datetime(pub_date)
+
+        if complete_date:
+            complete_date = Utils.string_to_datetime(complete_date)
+        self.complete_date = complete_date
