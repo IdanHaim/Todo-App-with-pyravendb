1af8704561c110cd83b61d0f07d2e1d4bca5aa2b
Idan Haim Shalom <haimdude@gmail.com>
2017-11-01 16:31:17 +0200

    Change home page to contains all todos, add uncompleted page, fix indentation

--- app.py
+++ app.py
@@ -3,52 +3,73 @@ from flask_paginate import Pagination, get_page_parameter
 from database import Database
 from models import Todo
 from datetime import datetime
-import re
 
-app = Flask(__name__)
-db = Database()
-
+app = Flask("PyRavenDB_Todo")
 POSTS_PER_PAGE = 10
+context = None
 
 
-@app.template_filter()
-def format_id(text):
-    return re.match(pattern=r"todos\/(\d+)-", string=text, flags=re.IGNORECASE).group(1)
+def run_app(debug=False):
+    global context
+    app.config["DATABASE_URLS"] = ["http://localhost:8080"]
+    app.config["DATABASE_NAME"] = "todo"
+    app.config["DEBUG"] = debug
+
+    context = app.app_context()
+    Database.create_database(context)
+    app.run()
+
+
+def get_store():
+    return Database.get_store(context)
 
 
 @app.route('/')
 def index():
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         page = request.args.get(get_page_parameter(), type=int, default=1)
 
-        incomplete_items, stats = list(session.query(with_statistics=True).order_by_descending(
-            "pub_date").where(done=False).take(POSTS_PER_PAGE).skip((page - 1) * POSTS_PER_PAGE))
+        todos, stats = list(session.query(object_type=Todo, with_statistics=True).order_by(
+            "done").order_by_descending("complete_date").order_by_descending("pub_date").take(POSTS_PER_PAGE).skip(
+            (page - 1) * POSTS_PER_PAGE))
         incomplete_total_result = stats['TotalResults']
 
-        pagination = Pagination(page=page, total=incomplete_total_result, record_name='incomplete todos')
-    return render_template('index.html', incomplete_items=incomplete_items, pagination=pagination)
+        pagination = Pagination(page=page, total=incomplete_total_result, record_name='todos')
+        return render_template('index.html', todos=todos, pagination=pagination)
 
 
 @app.route('/completed')
 def completed_items():
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         page = request.args.get(get_page_parameter(), type=int, default=1)
-        completed_items, stats = list(session.query(with_statistics=True).order_by_descending(
+        completed, stats = list(session.query(object_type=Todo, with_statistics=True).order_by_descending(
             "complete_date").where(done=True).take(POSTS_PER_PAGE).skip((page - 1) * POSTS_PER_PAGE))
         complete_total_result = stats['TotalResults']
 
         pagination = Pagination(page=page, total=complete_total_result, record_name='completed todos')
-    return render_template('completed.html', completed_items=completed_items, pagination=pagination)
+        return render_template('completed.html', completed_items=completed, pagination=pagination)
+
+
+@app.route('/uncompleted')
+def uncompleted():
+    with get_store().open_session() as session:
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        uncompleted, stats = list(session.query(object_type=Todo, with_statistics=True).order_by_descending(
+            "pub_date").where(done=False).take(POSTS_PER_PAGE).skip((page - 1) * POSTS_PER_PAGE))
+        incomplete_total_result = stats['TotalResults']
+
+        pagination = Pagination(page=page, total=incomplete_total_result, record_name='uncompleted todos')
+        return render_template('uncompleted.html', uncompleted=uncompleted, pagination=pagination)
 
 
 @app.route('/new', methods=['POST'])
 def create():
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         title = request.form['title']
         text = request.form['text']
         session.store(Todo(title=title, text=text))
         session.save_changes()
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
 
 @app.route('/new', methods=['GET'])
@@ -59,29 +80,30 @@ def new():
 @app.route('/complete')
 def complete():
     id = request.args.get('id')
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         todo = session.load(id)
         todo.done = True
         todo.complete_date = datetime.utcnow()
         session.save_changes()
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
 
 @app.route('/incomplete')
 def incomplete():
     id = request.args.get('id')
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         todo = session.load(id)
         todo.done = False
         todo.complete_date = None
         session.save_changes()
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
 
 @app.route('/todos', methods=['POST'])
 def update():
+    r = request
     id = request.args.get('id')
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         todo = session.load(id)
         todo.title = request.form['title']
         todo.text = request.form['text']
@@ -92,7 +114,7 @@ def update():
 @app.route('/todos', methods=['GET'])
 def view():
     id = request.args.get('id')
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         todo = session.load(id)
         return render_template('view.html', todo=todo)
 
@@ -100,11 +122,11 @@ def view():
 @app.route('/delete')
 def delete():
     id = request.args.get('id')
-    with db.document_store.open_session() as session:
+    with get_store().open_session() as session:
         session.delete(id)
         session.save_changes()
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    run_app(debug=False)
--- database.py
+++ database.py
@@ -1,27 +1,26 @@
 from pyravendb.store.document_store import DocumentStore
 from pyravendb.raven_operations.server_operations import CreateDatabaseOperation
 
-database_name = "todo"
-urls = ["http://localhost:8080"]
-
 
 class Database:
-    def __init__(self):
-        self._document_store = None
+    @staticmethod
+    def get_store(context):
+        store = getattr(context.g, "store", None)
+        if not store:
+            urls = context.app.config["DATABASE_URLS"]
+            database_name = context.app.config["DATABASE_NAME"]
+            store = DocumentStore(urls=urls, database=database_name)
+            store.initialize()
+            context.g.store = store
+        return store
 
-    @property
-    def document_store(self):
-        if self._document_store is None:
-            self._document_store = DocumentStore(urls=urls, database=database_name)
-            self.document_store.initialize()
-        self.create_database()
-        return self._document_store
-
-    def create_database(self):
-        try:
-            self._document_store.admin.server.send(CreateDatabaseOperation(database_name=database_name))
-        except Exception:
-            pass
-
-    def close(self):
-        self.document_store = None
+    @staticmethod
+    def create_database(context):
+        database_name = context.app.config["DATABASE_NAME"]
+        if not hasattr(context.g, database_name):
+            try:
+                store = Database.get_store(context)
+                setattr(context.g, database_name, True)
+                store.admin.server.send(CreateDatabaseOperation(database_name=database_name))
+            except:
+                pass
